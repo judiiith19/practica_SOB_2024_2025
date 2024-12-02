240d2333932751bdbf7ede90c210a799dea1f1d2
Judith García <judith.garcias@estudiants.urv.cat>
2024-12-03 00:33:39 +0100

    Modify RESTapp (new FacadeREST classes added)

--- src/java/service/RESTapp.java
+++ src/java/service/RESTapp.java
@@ -1,8 +1,18 @@
 package service;
 
 import jakarta.ws.rs.core.Application;
+import java.util.Set;
 
 @jakarta.ws.rs.ApplicationPath("webresources")
 public class RESTapp extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        
+        resources.add(ArticleFacadeREST.class);
+        resources.add(CustomerFacadeREST.class);
+        return resources;
+    }
     
 }
